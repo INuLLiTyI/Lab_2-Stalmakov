5ddd3e43aee21322aac70ad21d1091e36da8a869
INuLLiTyI <vovanstalmakov.ru19@gmail.com>
2025-10-14 23:09:02 +0300

    added template for extraction different types of objects from files(pipe ot CS)

--- main/InOut.h
+++ main/InOut.h
@@ -10,3 +10,13 @@ std::unordered_map<int, KS> KSCreate(std::unordered_map<int, KS>& m);
 std::unordered_map<int, Pipe> PipesCreate(std::unordered_map<int, Pipe>& m);
 void PipesPrint(std::unordered_map<int, Pipe>& m);
 void KSPrint(std::unordered_map<int, KS>& m);
+
+template <typename T>
+std::unordered_map<int, T> LoadObject(std::unordered_map<int, T>& Object, int count, std::ifstream& fin) {
+    for (int i = 0; i < count; i++) {
+        T obj{};
+        fin >> obj;
+        Object.emplace(obj.GetId(), obj);
+    }
+    return Object;
+}
