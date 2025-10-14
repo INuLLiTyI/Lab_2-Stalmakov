74baf10521362d5cba4c611d61eae340361f3847
INuLLiTyI <vovanstalmakov.ru19@gmail.com>
2025-10-14 23:05:47 +0300

    Added a header file for the connection/disconnection logic (InOut)

---
+++ main/InOut.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Pipe.h"
+#include "KS.h"
+#include <unordered_map>
+#include <sstream> 
+#include <vector>
+#include "Tools.h"
+
+std::unordered_map<int, KS> KSCreate(std::unordered_map<int, KS>& m);
+std::unordered_map<int, Pipe> PipesCreate(std::unordered_map<int, Pipe>& m);
+void PipesPrint(std::unordered_map<int, Pipe>& m);
+void KSPrint(std::unordered_map<int, KS>& m);
