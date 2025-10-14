63b944678983a09fb7536b50251d1f7d6cbd7c51
INuLLiTyI <vovanstalmakov.ru19@gmail.com>
2025-10-15 00:15:30 +0300

    Added new function declarations to Compressor_station.h

--- main/Compressor_station.h
+++ main/Compressor_station.h
@@ -3,37 +3,42 @@
 #include <fstream>
 #include <string>
 #include <istream>
-#include <unordered_map>
-#include <unordered_set>
 
 class CompressorStation {
-
 private:
     static int maxId;
     int id;
-    std::string Name;
-    int AmountOfWorkshops;
-    int WorkshopsInWork;
-    int EfficiencyLevel;
+    std::string name;
+    int workshops;
+    int workshopsInWork;
+    int efficiency;
 
 public:
     CompressorStation();
 
-
+    // Getters
     int GetId() const;
     static int GetMaxId();
     std::string GetName() const;
+    int GetWorkshops() const;
+    int GetWorkshopsInUse() const;
     int GetPercent() const;
 
+    // Setters
     void SetName(const std::string& name);
     void SetWorkshops(int total, int working);
     void SetEfficiency(int efficiency);
+
+    // Workshop management
     void StartWorkshop();
     void StopWorkshop();
+    bool UpdateWorkshopsInUse(int change);
+
     void Print() const;
 
     // Friend functions for I/O
     friend std::ostream& operator<<(std::ostream& out, const CompressorStation& cs);
     friend std::istream& operator>>(std::istream& in, CompressorStation& cs);
-
+    friend std::ifstream& operator>>(std::ifstream& fin, CompressorStation& g);
+    friend std::ofstream& operator<<(std::ofstream& fout, const CompressorStation& g);
 };
