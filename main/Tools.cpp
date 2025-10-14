6b4ddef6743c111456f35d11247119224e3b73d9
INuLLiTyI <vovanstalmakov.ru19@gmail.com>
2025-10-14 23:23:44 +0300

    Added filter functions to check existense of parameter for each existing object

--- main/Tools.cpp
+++ main/Tools.cpp
@@ -11,3 +11,21 @@ void fix() {
     std::cin.clear(); // Reset stream state
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 }
+
+bool checknamepipe(const Pipe& p, const std::string param)
+{
+    return p.Getname().find(param) != std::string::npos;
+}
+bool checkstate(const Pipe& p, bool param)
+{
+    return p.Getstate() == param;
+}
+
+bool checknameks(const KS& g, std::string param)
+{
+    return g.Getname().find(param) != std::string::npos;
+}
+bool workshops(const KS& g, int param)
+{
+    return g.Getworkshops() >= param;
+}
