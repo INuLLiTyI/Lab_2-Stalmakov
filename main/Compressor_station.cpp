df4f83d78d00d17dadd76a23482a26f660cec6b7
INuLLiTyI <vovanstalmakov.ru19@gmail.com>
2025-10-15 00:22:08 +0300

    Refactoring of Compressor_statiom.cpp for new logic of processing

--- main/Compressor_station.cpp
+++ main/Compressor_station.cpp
@@ -1,6 +1,7 @@
 #include "Compressor_station.h"
 #include <iostream>
-
+#include <fstream>
+#include "Tools.h"
 using namespace std;
 
 int CompressorStation::maxId = 0;
@@ -18,6 +19,8 @@ CompressorStation::CompressorStation()
 int CompressorStation::GetId() const { return id; }
 int CompressorStation::GetMaxId() { return maxId; }
 string CompressorStation::GetName() const { return Name; }
+int CompressorStation::GetWorkshops() const { return AmountOfWorkshops; }
+int CompressorStation::GetWorkshopsInUse() const { return WorkshopsInWork; }
 int CompressorStation::GetPercent() const {
     return (AmountOfWorkshops > 0) ? (WorkshopsInWork * 100 / AmountOfWorkshops) : 0;
 }
@@ -49,12 +52,24 @@ void CompressorStation::StopWorkshop() {
     }
 }
 
+bool CompressorStation::UpdateWorkshopsInUse(int change) {
+    int new_workshops_work = WorkshopsInWork + change;
+    if (new_workshops_work >= 0 && new_workshops_work <= AmountOfWorkshops) {
+        WorkshopsInWork = new_workshops_work;
+        return true;
+    }
+    else {
+        cout << "Error: number of working workshops must be between 0 and " << AmountOfWorkshops << ".\n";
+        return false;
+    }
+}
+
 void CompressorStation::Print() const {
     cout << "Compressor Station ID: " << id << endl;
     cout << "Name: " << Name << endl;
     cout << "Workshops: " << WorkshopsInWork << "/" << AmountOfWorkshops << " working" << endl;
     cout << "Efficiency Level: " << EfficiencyLevel << endl;
-    cout << "Utilization: " << GetPercent() << "%" << endl; 
+    cout << "Utilization: " << GetPercent() << "%" << endl;
 }
 
 ostream& operator<<(ostream& out, const CompressorStation& cs) {
@@ -72,4 +87,22 @@ istream& operator>>(istream& in, CompressorStation& cs) {
     getline(in, cs.Name);
     in >> cs.AmountOfWorkshops >> cs.WorkshopsInWork >> cs.EfficiencyLevel;
     return in;
+}
+
+ifstream& operator>>(ifstream& fin, CompressorStation& g) {
+    fin >> g.id;
+    fin >> ws;
+    getline(fin, g.Name);
+    fin >> g.AmountOfWorkshops;
+    fin >> g.WorkshopsInWork;
+    fin >> g.EfficiencyLevel;
+    if (g.id > g.maxId) {
+        g.maxId = g.id;
+    }
+    return fin;
+}
+
+ofstream& operator<<(ofstream& fout, const CompressorStation& g) {
+    fout << g.id << endl << g.Name << endl << g.AmountOfWorkshops << endl << g.WorkshopsInWork << endl << g.EfficiencyLevel << endl;
+    return fout;
 }
