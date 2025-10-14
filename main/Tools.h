bf156311cf1617aec3aa99c85a8d8e931e917795
INuLLiTyI <vovanstalmakov.ru19@gmail.com>
2025-10-14 23:36:40 +0300

    Added macro for correct reading of input arguments

--- main/Tools.h
+++ main/Tools.h
@@ -1,4 +1,4 @@
-?#pragma once
+﻿﻿#pragma once
 #include <iostream>
 #include "Pipe.h"
 #include "KS.h"
@@ -6,3 +6,6 @@
 #include <unordered_set>
 #include <chrono>
 #include <format>
+
+#define INPUT_LINE(in, str) getline(in>>std::ws, str); \
+						std::cerr << str << std::endl
