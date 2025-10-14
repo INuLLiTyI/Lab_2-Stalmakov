87b126b6d619756a69023a6c96f9a864eaaa8e45
INuLLiTyI <vovanstalmakov.ru19@gmail.com>
2025-10-14 23:39:53 +0300

    Added feature for logging errors (class redirect_output_wrapper)

--- main/Tools.h
+++ main/Tools.h
@@ -9,3 +9,22 @@
 
 #define INPUT_LINE(in, str) getline(in>>std::ws, str); \
 						std::cerr << str << std::endl
+void fix();
+
+class redirect_output_wrapper
+{
+	std::ostream& stream;
+	std::streambuf* const old_buf;
+public:
+	redirect_output_wrapper(std::ostream& src)
+		:old_buf(src.rdbuf()), stream(src)
+	{
+	}
+	~redirect_output_wrapper() {
+		stream.rdbuf(old_buf);
+	}
+	void redirect(std::ostream& dest)
+	{
+		stream.rdbuf(dest.rdbuf());
+	}
+};
