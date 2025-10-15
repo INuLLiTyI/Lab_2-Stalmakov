a2bcce3cf2f927e9351d369a2220fcf6f8254651
INuLLiTyI <vovanstalmakov.ru19@gmail.com>
2025-10-15 03:11:40 +0300

    keep local changes

--- main/Compressor_station.cpp
+++ main/Compressor_station.cpp
@@ -1,46 +1,5 @@
 #include "Compressor_station.h"
 #include <iostream>
-<<<<<<< HEAD
-#include <fstream>
-#include "Tools.h"
-using namespace std;
-
-int CompressorStation::maxId = 0;
-
-CompressorStation::CompressorStation()
-{
-    id = ++maxId;
-    Name = "None";
-    AmountOfWorkshops = 0;
-    WorkshopsInWork = 0;
-    EfficiencyLevel = 0;
-}
-
-
-int CompressorStation::GetId() const { return id; }
-int CompressorStation::GetMaxId() { return maxId; }
-string CompressorStation::GetName() const { return Name; }
-int CompressorStation::GetWorkshops() const { return AmountOfWorkshops; }
-int CompressorStation::GetWorkshopsInUse() const { return WorkshopsInWork; }
-int CompressorStation::GetPercent() const {
-    return (AmountOfWorkshops > 0) ? (WorkshopsInWork * 100 / AmountOfWorkshops) : 0;
-}
-
-void CompressorStation::SetName(const string& name) { Name = name; }
-void CompressorStation::SetWorkshops(int total, int working) {
-    AmountOfWorkshops = total;
-    WorkshopsInWork = working;
-}
-void CompressorStation::SetEfficiency(int efficiency) { EfficiencyLevel = efficiency; }
-
-void CompressorStation::StartWorkshop() {
-    if (WorkshopsInWork < AmountOfWorkshops) {
-        WorkshopsInWork++;
-        cout << "Workshop started. Now working: " << WorkshopsInWork << "/" << AmountOfWorkshops << endl;
-    }
-    else {
-        cout << "All workshops are already working!\n";
-=======
 #include <string>
 #include <fstream>
 #include <format>
@@ -48,9 +7,8 @@ void CompressorStation::StartWorkshop() {
 // Инициализация статической переменной
 int CompressorStation::maxId = 0;
 
-CompressorStation::CompressorStation()
-    : id(++maxId), name(""), workshops(0), workshopsInWork(0), efficiency(0) {
-}
+CompressorStation::CompressorStation() 
+    : id(++maxId), name(""), workshops(0), workshopsInWork(0), efficiency(0) {}
 
 // Getters
 int CompressorStation::GetId() const {
@@ -99,79 +57,16 @@ void CompressorStation::SetEfficiency(int newEfficiency) {
 void CompressorStation::StartWorkshop() {
     if (workshopsInWork < workshops) {
         workshopsInWork++;
->>>>>>> 3323d9974e07d2145f6e6e28374ffa222fb0faf7
     }
 }
 
 void CompressorStation::StopWorkshop() {
-<<<<<<< HEAD
-    if (WorkshopsInWork > 0) {
-        WorkshopsInWork--;
-        cout << "Workshop stopped. Now working: " << WorkshopsInWork << "/" << AmountOfWorkshops << endl;
-    }
-    else {
-        cout << "No workshops are working!\n";
-=======
     if (workshopsInWork > 0) {
         workshopsInWork--;
->>>>>>> 3323d9974e07d2145f6e6e28374ffa222fb0faf7
     }
 }
 
 bool CompressorStation::UpdateWorkshopsInUse(int change) {
-<<<<<<< HEAD
-    int new_workshops_work = WorkshopsInWork + change;
-    if (new_workshops_work >= 0 && new_workshops_work <= AmountOfWorkshops) {
-        WorkshopsInWork = new_workshops_work;
-        return true;
-    }
-    else {
-        cout << "Error: number of working workshops must be between 0 and " << AmountOfWorkshops << ".\n";
-        return false;
-    }
-}
-
-void CompressorStation::Print() const {
-    cout << "Compressor Station ID: " << id << endl;
-    cout << "Name: " << Name << endl;
-    cout << "Workshops: " << WorkshopsInWork << "/" << AmountOfWorkshops << " working" << endl;
-    cout << "Efficiency Level: " << EfficiencyLevel << endl;
-    cout << "Utilization: " << GetPercent() << "%" << endl;
-}
-
-ostream& operator<<(ostream& out, const CompressorStation& cs) {
-    out << cs.id << endl;
-    out << cs.Name << endl;
-    out << cs.AmountOfWorkshops << endl;
-    out << cs.WorkshopsInWork << endl;
-    out << cs.EfficiencyLevel << endl;
-    return out;
-}
-
-istream& operator>>(istream& in, CompressorStation& cs) {
-    in >> cs.id;
-    in.ignore();
-    getline(in, cs.Name);
-    in >> cs.AmountOfWorkshops >> cs.WorkshopsInWork >> cs.EfficiencyLevel;
-    return in;
-}
-
-ifstream& operator>>(ifstream& fin, CompressorStation& g) {
-    fin >> g.id;
-    fin >> ws;
-    getline(fin, g.Name);
-    fin >> g.AmountOfWorkshops;
-    fin >> g.WorkshopsInWork;
-    fin >> g.EfficiencyLevel;
-    if (g.id > g.maxId) {
-        g.maxId = g.id;
-    }
-    return fin;
-}
-
-ofstream& operator<<(ofstream& fout, const CompressorStation& g) {
-    fout << g.id << endl << g.Name << endl << g.AmountOfWorkshops << endl << g.WorkshopsInWork << endl << g.EfficiencyLevel << endl;
-=======
     int newValue = workshopsInWork + change;
     if (newValue >= 0 && newValue <= workshops) {
         workshopsInWork = newValue;
@@ -185,7 +80,7 @@ void CompressorStation::Print() const {
     std::cout << std::format("  Total workshops: {}\n", workshops);
     std::cout << std::format("  Workshops in work: {}\n", workshopsInWork);
     std::cout << std::format("  Efficiency: {}\n", efficiency);
-
+    
     if (workshops > 0) {
         double percentage = (static_cast<double>(workshopsInWork) / workshops) * 100;
         std::cout << std::format("  Percentage of workshops in work: {:.2f}%\n", percentage);
@@ -203,16 +98,16 @@ std::ostream& operator<<(std::ostream& out, const CompressorStation& cs) {
 std::istream& operator>>(std::istream& in, CompressorStation& cs) {
     std::cout << "Enter compressor station name: ";
     std::getline(in, cs.name);
-
+    
     std::cout << "Enter total number of workshops: ";
     in >> cs.workshops;
-
+    
     std::cout << "Enter number of workshops in work: ";
     in >> cs.workshopsInWork;
-
+    
     std::cout << "Enter efficiency: ";
     in >> cs.efficiency;
-
+    
     in.ignore(); // очистка буфера
     return in;
 }
@@ -233,6 +128,5 @@ std::ofstream& operator<<(std::ofstream& fout, const CompressorStation& cs) {
     fout << cs.workshops << "\n";
     fout << cs.workshopsInWork << "\n";
     fout << cs.efficiency << "\n";
->>>>>>> 3323d9974e07d2145f6e6e28374ffa222fb0faf7
     return fout;
 }
--- main/pipe.cpp
+++ main/pipe.cpp
@@ -11,39 +11,15 @@ int Pipe::maxId = 0;
 Pipe::Pipe()
 {
     id = ++maxId;
-<<<<<<< HEAD
-    Name = "None";
-    Length = 0.0f;
-    Diameter = 0;
-    InRepair = false;
-=======
     name = "None";
     length = 0.0f;
     diameter = 0;
     inRepair = false;
->>>>>>> 3323d9974e07d2145f6e6e28374ffa222fb0faf7
     connectedStationId = 0;
 }
 
 int Pipe::GetId() const { return id; }
 int Pipe::GetMaxId() { return maxId; }
-<<<<<<< HEAD
-string Pipe::GetName() const { return Name; }
-bool Pipe::GetState() const { return InRepair; }
-float Pipe::GetLength() const { return Length; }
-int Pipe::GetDiameter() const { return Diameter; }
-bool Pipe::IsConnected() const { return connectedStationId != 0; }
-int Pipe::GetConnectedStationId() const { return connectedStationId; }
-
-void Pipe::SetName(const string& name) { Name = name; }
-void Pipe::SetLength(float length) { Length = length; }
-void Pipe::SetDiameter(int diameter) { Diameter = diameter; }
-void Pipe::SetInRepair(bool inRepair) { InRepair = inRepair; }
-
-void Pipe::ConnectToStation(int stationId) {
-    connectedStationId = stationId;
-    InRepair = false;
-=======
 string Pipe::GetName() const { return name; }
 bool Pipe::GetState() const { return inRepair; }
 float Pipe::GetLength() const { return length; }
@@ -59,22 +35,10 @@ void Pipe::SetInRepair(bool repairStatus) { inRepair = repairStatus; }
 void Pipe::ConnectToStation(int stationId) {
     connectedStationId = stationId;
     inRepair = false;
->>>>>>> 3323d9974e07d2145f6e6e28374ffa222fb0faf7
 }
 
 void Pipe::Disconnect() {
     connectedStationId = 0;
-<<<<<<< HEAD
-    InRepair = true;
-}
-
-void Pipe::changeofstate() {
-    InRepair = !InRepair;
-}
-
-void Pipe::Edit() {
-    cout << "Edit pipe (current repair status: " << (InRepair ? "in repair" : "working") << "):\n";
-=======
     inRepair = true;
 }
 
@@ -84,7 +48,6 @@ void Pipe::changeofstate() {
 
 void Pipe::Edit() {
     cout << "Edit pipe (current repair status: " << (inRepair ? "in repair" : "working") << "):\n";
->>>>>>> 3323d9974e07d2145f6e6e28374ffa222fb0faf7
     cout << "Set repair status (0 - working, 1 - in repair): ";
     int status;
     cin >> status;
@@ -94,47 +57,27 @@ void Pipe::Edit() {
         cout << "Invalid input! Status unchanged.\n";
     }
     else {
-<<<<<<< HEAD
-        InRepair = (status == 1);
-=======
         inRepair = (status == 1);
->>>>>>> 3323d9974e07d2145f6e6e28374ffa222fb0faf7
     }
 }
 
 void Pipe::Print() const {
     cout << "Pipe ID: " << id << endl;
-<<<<<<< HEAD
-    cout << "Name: " << Name << endl;
-    cout << fixed << setprecision(2);
-    cout << "Length: " << Length << " km" << endl;
-    cout << "Diameter: " << Diameter << " mm" << endl;
-    cout << "Status: " << (InRepair ? "In repair" : "Operational") << endl;
-=======
     cout << "Name: " << name << endl;
     cout << fixed << setprecision(2);
     cout << "Length: " << length << " km" << endl;
     cout << "Diameter: " << diameter << " mm" << endl;
     cout << "Status: " << (inRepair ? "In repair" : "Operational") << endl;
->>>>>>> 3323d9974e07d2145f6e6e28374ffa222fb0faf7
     cout << "Connected: " << (IsConnected() ? "True" : "False") << endl;
 }
 
 ostream& operator<<(ostream& out, const Pipe& pipe) {
     out << pipe.id << endl;
-<<<<<<< HEAD
-    out << pipe.Name << endl;
-    out << fixed << setprecision(6);
-    out << pipe.Length << endl;
-    out << pipe.Diameter << endl;
-    out << pipe.InRepair << endl;
-=======
     out << pipe.name << endl;
     out << fixed << setprecision(6);
     out << pipe.length << endl;
     out << pipe.diameter << endl;
     out << pipe.inRepair << endl;
->>>>>>> 3323d9974e07d2145f6e6e28374ffa222fb0faf7
     out << pipe.connectedStationId << endl;
     return out;
 }
@@ -142,30 +85,18 @@ ostream& operator<<(ostream& out, const Pipe& pipe) {
 istream& operator>>(istream& in, Pipe& pipe) {
     in >> pipe.id;
     in.ignore();
-<<<<<<< HEAD
-    getline(in, pipe.Name);
-    in >> pipe.Length >> pipe.Diameter >> pipe.InRepair >> pipe.connectedStationId;
-=======
     getline(in, pipe.name);
     in >> pipe.length >> pipe.diameter >> pipe.inRepair >> pipe.connectedStationId;
->>>>>>> 3323d9974e07d2145f6e6e28374ffa222fb0faf7
     return in;
 }
 
 ifstream& operator>>(ifstream& fin, Pipe& p) {
     fin >> p.id;
     fin >> std::ws;
-<<<<<<< HEAD
-    getline(fin, p.Name);
-    fin >> p.Length;
-    fin >> p.Diameter;
-    fin >> p.InRepair;
-=======
     getline(fin, p.name);
     fin >> p.length;
     fin >> p.diameter;
     fin >> p.inRepair;
->>>>>>> 3323d9974e07d2145f6e6e28374ffa222fb0faf7
     fin >> p.connectedStationId;
     if (p.id > p.maxId) {
         p.maxId = p.id;
@@ -175,17 +106,10 @@ ifstream& operator>>(ifstream& fin, Pipe& p) {
 
 ofstream& operator<<(ofstream& fout, const Pipe& p) {
     fout << p.id << endl
-<<<<<<< HEAD
-        << p.Name << endl
-        << p.Length << endl
-        << p.Diameter << endl
-        << p.InRepair << endl
-=======
         << p.name << endl
         << p.length << endl
         << p.diameter << endl
         << p.inRepair << endl
->>>>>>> 3323d9974e07d2145f6e6e28374ffa222fb0faf7
         << p.connectedStationId << endl;
     return fout;
 }
